26acd95a90d484ef7df7c42be655643f36334fc2
HassanSheikh456 <hassan.nadeem.4012@gmail.com>
2026-02-22 02:01:57 +0500

    style: format User component code for consistency and readability

--- src/components/User/User.jsx
+++ src/components/User/User.jsx
@@ -1,13 +1,11 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
+import React from "react";
+import { useParams } from "react-router-dom";
 
 const User = () => {
-    const { userid } = useParams()
+  const { userid } = useParams();
   return (
-    <div className='bg-gray-700 text-2xl text-white p-3'>
-      User: {userid}
-    </div>
-  )
-}
+    <div className="bg-gray-700 text-2xl text-white p-3">User: {userid}</div>
+  );
+};
 
-export default User
+export default User;
