2a2a13c8b45a66026840dab9435b86838340379a
HassanSheikh456 <hassan.nadeem.4012@gmail.com>
2026-02-22 01:15:31 +0500

    style: update User component to enhance styling with Tailwind CSS

--- src/components/User/User.jsx
+++ src/components/User/User.jsx
@@ -4,7 +4,7 @@ import { useParams } from 'react-router-dom'
 const User = () => {
     const { userid } = useParams()
   return (
-    <div>
+    <div className='bg-gray-700 text-2xl text-white p-3'>
       User: {userid}
     </div>
   )
